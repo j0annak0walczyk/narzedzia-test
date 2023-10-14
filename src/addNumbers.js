9c314d4c690797a136f5c954e957f5b9d7d346b0
j0annak0walczyk <j.kowalczyk0070@gmail.com>
2023-10-14 16:26:43 +0200

    Mój pierwszy commit

---
+++ src/addNumbers.js
@@ -0,0 +1,5 @@
+// const addTwoNumbers = (num1, num2) => num1 + num2;
+
+// const [liczba1, liczba2] = process.argv.slice(2);
+// const asia = addTwoNumbers(+liczba1, +liczba2);
+// console.log(asia);
---
+++ src/script.js
@@ -0,0 +1,31 @@
+// const addTwoNumbers = (num1, num2) => num1 + num2;
+
+// const [liczba1, liczba2] = process.argv.slice(2);
+// const asia = addTwoNumbers(+liczba1, +liczba2);
+// console.log(asia);
+import Chart from "chart.js/auto";
+
+(async function () {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  new Chart(document.getElementById("acquisitions"), {
+    type: "bar",
+    data: {
+      labels: data.map((row) => row.year),
+      datasets: [
+        {
+          label: "Acquisitions by year",
+          data: data.map((row) => row.count),
+        },
+      ],
+    },
+  });
+})();
